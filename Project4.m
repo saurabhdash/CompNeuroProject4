103ada7bf6e37b510689153c3396a18ccccb5f43
saurabhdash <saurabhdash2512@gmail.com>
2017-11-17 20:50:14 +0530

    Part2

--- Project4.m
+++ Project4.m
@@ -28,38 +28,70 @@ clc;
 % ylabel('Mean Square Error');
 % xlabel('# of Repetitions');
 % figure;
-%%
+%% Part 1
 % clearvars p lamda;
-p.ns = 0.5/1000*ones(1,250);
-p.s = 10/1000*ones(1,50);
-p.s1 = 2.5/1000*ones(1,50);
-lamda.S = [p.s,p.ns, p.s,p.ns, p.s,p.ns, p.s,p.ns...
-    , p.s,p.ns, p.s,p.ns, p.s,p.ns, p.s1,p.ns, p.s,p.ns, p.s,p.ns...
-    , p.s,p.ns, p.s,p.ns, p.s,p.ns, p.s,p.ns, p.s,p.ns];
-lamda.D = [p.s1,p.ns, p.s1,p.ns, p.s1,p.ns, p.s1,p.ns...
-    , p.s1,p.ns, p.s1,p.ns, p.s1,p.ns, p.s,p.ns, p.s1,p.ns, p.s1,p.ns...
-    , p.s1,p.ns, p.s1,p.ns, p.s1,p.ns, p.s1,p.ns, p.s1,p.ns];
-Input.s = zeros(1,length(lamda.S));
-Input.d = zeros(1,length(lamda.D));
-for j = 1:length(lamda.S)
-        Input.s(j) = binornd(1,lamda.S(j));
-        Input.d(j) = binornd(1,lamda.D(j));
-end
+[Spikes,L] =  GenSpike();
+[Spikes,V,g] = verNoP(Spikes,L);
+%Plot
+figure(3);
+subplot(4,1,1);
+plot(Spikes.s);
+xlabel('t(ms)');
+ylabel('S');
+ylim([0 1]);
+subplot(4,1,2);
+plot(Spikes.d);
+xlabel('t(ms)');
+ylabel('D');
+ylim([0 1]);
+subplot(4,1,3);
+plot(Spikes.sp);
+xlabel('t(ms)');
+ylabel('SP');
+ylim([0 1]);
+subplot(4,1,4);
+plot(Spikes.l4);
+xlabel('t(ms)');
+ylabel('L4');
+ylim([0 1]);
+% figure(4);
 % subplot(2,1,1);
-% plot(Input.s);
-% title('S');
+% plot(V.sp);
 % xlabel('t(ms)');
-% ylabel('Spikes');
-% ylim([0 1]);
+% ylabel('V_SP');
 % subplot(2,1,2);
-% plot(Input.d);
-% title('D');
+% plot(V.l4);
 % xlabel('t(ms)');
-% ylabel('Spikes');
-% ylim([0 1]);
-Spikes = Input;
-Spikes.sp = zeros(1,length(lamda.S));
-Spikes.l4 = zeros(1,length(lamda.S));
+% ylabel('V_L4');
+% 
+% figure(5);
+% plot(g.s);
+% xlabel('t(ms)');
+% ylabel('g.s');
+%%Part 2
+PSTH.sp = zeros(1,L);
+PSTH.l4 = zeros(1,L);
+for i = 1:50
+    [Spikes,L] =  GenSpike();
+    [Spikes,V,g] = verNoP(Spikes,L);
+    PSTH.sp = PSTH.sp + Spikes.sp;
+    PSTH.l4 = PSTH.l4 + Spikes.l4;
+end
+PSTH.sp = PSTH.sp * 1000/50;
+PSTH.l4 = PSTH.l4 * 1000/50;
+figure(4)
+subplot(2,1,1)
+plot(PSTH.sp)
+xlabel('t(ms)');
+ylabel('PSTH_S_P');
+subplot(2,1,2)
+plot(PSTH.l4)
+xlabel('t(ms)');
+ylabel('PSTH_L_4');
+
+function [Spikes,V,g] = verNoP(Spikes,L)
+Spikes.sp = zeros(1,L);
+Spikes.l4 = zeros(1,L);
 %% Model Parameters
 % Initial weights
 w_init.s_sp = 0.2;
@@ -97,19 +129,19 @@ tau.syn = 10;
 beta = 5;
 tau.ref = 2;
 %%
-g.s = zeros(1,length(lamda.S));
-g.d = zeros(1,length(lamda.S));
-g.sp = zeros(1,length(lamda.S));
-V.sp = zeros(1,length(lamda.S));
-V.l4 = zeros(1,length(lamda.S));
+g.s = zeros(1,L);
+g.d = zeros(1,L);
+g.sp = zeros(1,L);
+V.sp = zeros(1,L);
+V.l4 = zeros(1,L);
 %%
-for t = 2:length(lamda.S)
+for t = 2:L
     if(Spikes.s(t-1))
-        g.s(t:end) = g.s(t-1) + exp(-((t:length(lamda.S)) - t)/tau.syn);
+        g.s(t:end) = g.s(t-1) + exp(-((t:L) - t)/tau.syn);
     elseif(Spikes.d(t-1))
-        g.d(t:end) = g.d(t-1) + exp(-((t:length(lamda.S)) - t)/tau.syn);
+        g.d(t:end) = g.d(t-1) + exp(-((t:L) - t)/tau.syn);
     elseif(Spikes.sp(t-1))
-        g.sp(t:end) = g.sp(t-1) + exp(-((t:length(lamda.S)) - t)/tau.syn);
+        g.sp(t:end) = g.sp(t-1) + exp(-((t:L) - t)/tau.syn);
     end
     
     V.sp(t) = V.sp(t) + g.s(t-1)*w.s_sp*x.s.e + g.d(t-1)*w.d_sp*x.d.e;
@@ -128,41 +160,37 @@ for t = 2:length(lamda.S)
     end
     x = ShortTermPlasticity(x,tau,Spikes.s(t),Spikes.d(t),Spikes.sp(t));
 end
-figure(3);
-subplot(4,1,1);
-plot(Spikes.s);
-xlabel('t(ms)');
-ylabel('S');
-ylim([0 1]);
-subplot(4,1,2);
-plot(Spikes.d);
-xlabel('t(ms)');
-ylabel('D');
-ylim([0 1]);
-subplot(4,1,3);
-plot(Spikes.sp);
-xlabel('t(ms)');
-ylabel('SP');
-ylim([0 1]);
-subplot(4,1,4);
-plot(Spikes.l4);
-xlabel('t(ms)');
-ylabel('L4');
-ylim([0 1]);
-% figure(4);
+end
+function [Input,L] = GenSpike()
+p.ns = 0.5/1000*ones(1,250);
+p.s = 10/1000*ones(1,50);
+p.s1 = 2.5/1000*ones(1,50);
+lamda.S = [p.s,p.ns, p.s,p.ns, p.s,p.ns, p.s,p.ns...
+    , p.s,p.ns, p.s,p.ns, p.s,p.ns, p.s1,p.ns, p.s,p.ns, p.s,p.ns...
+    , p.s,p.ns, p.s,p.ns, p.s,p.ns, p.s,p.ns, p.s,p.ns];
+lamda.D = [p.s1,p.ns, p.s1,p.ns, p.s1,p.ns, p.s1,p.ns...
+    , p.s1,p.ns, p.s1,p.ns, p.s1,p.ns, p.s,p.ns, p.s1,p.ns, p.s1,p.ns...
+    , p.s1,p.ns, p.s1,p.ns, p.s1,p.ns, p.s1,p.ns, p.s1,p.ns];
+L = length(lamda.S);
+Input.s = zeros(1,L);
+Input.d = zeros(1,length(lamda.D));
+for j = 1:L
+        Input.s(j) = binornd(1,lamda.S(j));
+        Input.d(j) = binornd(1,lamda.D(j));
+end
 % subplot(2,1,1);
-% plot(V.sp);
+% plot(Input.s);
+% title('S');
 % xlabel('t(ms)');
-% ylabel('V_SP');
+% ylabel('Spikes');
+% ylim([0 1]);
 % subplot(2,1,2);
-% plot(V.l4);
+% plot(Input.d);
+% title('D');
 % xlabel('t(ms)');
-% ylabel('V_L4');
-% 
-% figure(5);
-% plot(g.s);
-% xlabel('t(ms)');
-% ylabel('g.s');
+% ylabel('Spikes');
+% ylim([0 1]);
+end
 function y = ShortTermPlasticity(x,tau,s1,s2,s3)
 y.s.r = x.s.r - s1*x.s.r/tau.th.re + x.s.i/tau.th.ir;
 y.s.e = x.s.e + s1*x.s.r/tau.th.re - x.s.e/tau.th.ei;
